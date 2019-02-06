d5e68b29a60357f9ff3e3a668c33a0a4eb709f9c
ratskin <elliot.schep@gmail.com>
2019-02-06 23:17:15 +0100

    Updates

---
+++ precache-manifest.6069920f7eb0b502de73f853c440ff67.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec93aec20373ae8defaf",
+    "url": "/movie-night/static/css/main.03c4e1af.chunk.css"
+  },
+  {
+    "revision": "ec93aec20373ae8defaf",
+    "url": "/movie-night/static/js/main.ec93aec2.chunk.js"
+  },
+  {
+    "revision": "35c8273ea40ccde317d0",
+    "url": "/movie-night/static/js/1.35c8273e.chunk.js"
+  },
+  {
+    "revision": "43a1afb4642853d73d50",
+    "url": "/movie-night/static/js/runtime~main.43a1afb4.js"
+  },
+  {
+    "revision": "2436b334fb8be217ea70887272ff316a",
+    "url": "/movie-night/index.html"
+  }
+];
